2aba7a28449b5d0b12152315b6d7073438bba132
aoi-netai <aoi256jp@Gmail.com>
2026-03-29 03:25:11 +0000

    所有権を少し勉強

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
-}
+    
+    // バイナリ（実行ファイルのデータ領域）への確保
+    let s1: &str = "message1";
+    println!("string_s1: {}", s1);
+
+    // s1が所有権をもっているわけではないので、これはコピーが可能（内部処理されてる）
+    // s1, s2両方とも使える
+    let s2: &str = s1;
+    println!("string_s2: {}", s2);
+    println!("string_s1: {}", s1);
+
+    // ヒープ上への確保
+    let s3: String = String::from("message2");
+    println!("string_s3: {}", s3);
+
+    // 所有権を移しているので、ここでs3は破棄される
+    // s3を使うことはできない
+    let s4: String = s3;
+    println!("string_s4: {}", s4);
+    //println!("string_s3: {}", s3); // エラー
+
+    // s4に所有権を持たせたまま、s5にコピー（s5はコピーされた変数の所有権を持つ）
+    let s5: String = s4.clone();
+    println!("string_s4: {}", s4);
+    println!("string_s5: {}", s5);
+
+    // s4に所有権を持たせたまま、s4の参照を受け取る（s6は参照のみで、所有権を持っていない）
+    let s6: &String = &s4;
+    println!("string_s6: {}", s6);
+
+
+}       
